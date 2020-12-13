f73407791ec34b7c0d2095a062293c4e68e2129e
Ali Daniel Febres T <alidanft@gmail.com>
2020-12-13 13:21:49 +0100

    fix: deployment issue

--- server/src/index.js
+++ server/src/index.js
@@ -44,15 +44,6 @@ app.get("/api/phone/:id", async (req, res) => {
   }
 });
 
-if (process.env.NODE_ENV === "production") {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, "../client/build")));
-  // Handle React routing, return all requests to React app
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-  });
-}
-
 app.all("*", (_, res) => {
   try {
     throw new Error("Bad request");
